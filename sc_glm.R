688782eccd5e62e75052a28e9b8cb4ff658b3bf7
nar <d.narsil@gmail.com>
2015-02-06 01:43:16 -0500

    Add significance test

--- sc_glm.R
+++ sc_glm.R
@@ -31,8 +31,12 @@ db <- 'mj.sqlite3'
 source('sc_tables.R')
 
 
-out_path <- paste('output/sc_glm_', gsub('[ :]', '-', Sys.time()), '.txt', sep = '')
-cat('glm\n', file = out_path)
+stdout_path <- paste('output/sc_glm_', gsub('[ :]', '-', Sys.time()), '.txt', sep = '')
+signal_path <- 'output/sc_glm_signal.txt'
+or_csv_path <- 'output/sc_glm_orci.csv'
+cat('glm\n', file = stdout_path)
+cat('glm signal\n', file = signal_path)
+dt_orci <- data.table()
 
 foreach (code = dt_hlts$hlt_code, .packages = pkgs) %dopar% {
   hlt <- dt_hlts %>% filter(hlt_code == code)
@@ -48,30 +52,37 @@ foreach (code = dt_hlts$hlt_code, .packages = pkgs) %dopar% {
           mutate(concomit = ifelse(is.na(concomit), 0, concomit)) %>%
           mutate(event = as.integer(ifelse(case_id %in% reac$case_id, 1, 0)))
 
-  e <- try(fit <- glm(event ~ incretin +
-                              concomit +
-                              age +
-                              sex,
-                      data = dt, family = binomial),
-           silent = FALSE)
+  fit <- glm(event ~ incretin +
+                     concomit +
+                     age +
+                     sex,
+             data = dt, family = binomial)
 
-  sink(file = out_path, append = TRUE)
-    if (class(e) != 'try-error') {
-      s <- summary(fit)
-      ci <- confint(fit, level = 0.99)
-      ors <- exp(cbind(s$coefficients[,1], ci[,1:2]))
-      colnames(ors) <- c('OR', 'LL99', 'UL99')
-      p <- list(event = t(hlt),
-                summary = s,
-                odds_ratio = ors)
-      cat('\n\n\n')
-      print(p)
-    } else {
-      p <- list(event = t(hlt),
-                summary = 'ERROR')
-      cat('\n\n\n')
-      print(p)
-      warning()
-    }
+  s <- summary(fit)
+  ci <- confint(fit, level = 0.99)
+  ors <- exp(cbind(s$coefficients[,1], ci[,1:2]))
+  colnames(ors) <- c('OR', 'LL', 'UL')
+  p <- list(event = t(hlt),
+            summary = s,
+            odds_ratio = ors)
+
+  sink(file = stdout_path, append = TRUE)
+    cat('\n\n\n')
+    print(p)
   sink()
+  if (ors[2,2] > 1) {
+    p <- list(p,
+              incretin_or = paste('HLT', hlt$hlt_code, ';', ors[2,1], '[', ors[2,2], '-', ors[2,3], ']'),
+              count = hlt$case_count,
+              hlt = paste(hlt$hlt_name, hlt$hlt_kanji))
+
+    sink(file = signal_path, append = TRUE)
+      cat('\n\n\n')
+      print(p)
+    sink()
+
+    dt_orci <- dt_orci %>% rbind(c(ors[2,], c(hlt)))
+  }
 }
+
+write.csv(dt_orci, file = or_csv_path, row.names = FALSE)
