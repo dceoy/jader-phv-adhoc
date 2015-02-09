990474b4b83e2e96783d0af500d91cacb7786014
dceoy <d.narsil@gmail.com>
2015-02-10 02:04:20 +0900

    Optimize

--- stan.R
+++ stan.R
@@ -27,8 +27,17 @@ select <- dplyr::select
 cl <- makeCluster(detectCores(), type = 'SOCK')
 registerDoSNOW(cl)
 .Last <- function() stopCluster(cl)
+
 db <- 'mj.sqlite3'
-source('tables.R')
+source('dm_tbl.R')
+#      NAME      NROW NCOL MB COLS                                                  KEY
+# [1,] dt_base  6,897    6  1 case_id,suspected,age,sex,dpp4_inhibitor,glp1_agonist
+# [2,] dt_ccmt 55,700    2  2 case_id,drug
+# [3,] dt_hist 29,492    2  1 case_id,hlt_code
+# [4,] dt_hlts    628    4  1 hlt_code,hlt_name,hlt_kanji,case_count
+# [5,] dt_reac  8,011    2  1 case_id,hlt_code
+# [6,] dt_sgnl 70,930    2  2 drug,hlt_code
+# Total: 8MB
 
 out_path <- paste('output/stan_', gsub('[ :]', '-', Sys.time()), '.txt', sep = '')
 cat('stan\n', file = out_path)
@@ -40,6 +49,7 @@ st_model <- stan_model(file = 'ae.stan')
 foreach (code = hlt_codes) %do% {
   hlt <- dt_hlts %>% filter(hlt_code == code)
   reac <- dt_reac %>% filter(hlt_code == code)
+  hist <- dt_hist %>% filter(hlt_code == code)
   sgnl <- dt_sgnl %>% filter(hlt_code == code)
   ccmt <- dt_ccmt %>%
             filter(drug %in% sgnl$drug) %>%
@@ -48,15 +58,16 @@ foreach (code = hlt_codes) %do% {
 
   dt <- dt_base %>%
           left_join(ccmt, by = 'case_id') %>%
-          mutate(concomit = ifelse(is.na(concomit), 0, concomit)) %>%
-          mutate(event = ifelse(case_id %in% reac$case_id, 1, 0)) %>%
-          select(event, dpp4_inhibitor, glp1_agonist, concomit, age, sex, suspected)
+          mutate(concomit = as.integer(ifelse(is.na(concomit), 0, concomit))) %>%
+          mutate(preexist = as.integer(ifelse(case_id %in% hist$case_id, 1, 0))) %>%
+          mutate(event = as.integer(ifelse(case_id %in% reac$case_id, 1, 0))) %>%
+          select(event, dpp4_inhibitor, glp1_agonist, concomit, preexist, age, sex, suspected)
 
   ae_dat <- list(N = dt %>% nrow(),
-                 M = 5,
+                 M = 6,
                  L = dt %>% distinct(suspected) %>% nrow(),
-                 y = dt %>% .$event,
-                 x = dt %>% select(dpp4_inhibitor, glp1_agonist, concomit, age, sex),
+                 y = dt$event,
+                 x = dt %>% select(dpp4_inhibitor, glp1_agonist, concomit, preexist, age, sex),
                  g = dt %>%
                        distinct(suspected) %>%
                        mutate(s_id = 1:nrow(.)) %>%
@@ -66,15 +77,17 @@ foreach (code = hlt_codes) %do% {
 
 # stanfit <- sampling(object = st_model, data = ae_dat, iter = 1000, chains = 4)
   sflist <- foreach(i = 1:4, .packages = 'rstan') %dopar% {
-              sampling(object = st_model, data = ae_dat, iter = 2000, chains = 1, chain_id = i, refresh = -1)
+              sampling(object = st_model, data = ae_dat, iter = 1000, chains = 1, chain_id = i, refresh = -1)
             }
   stanfit <- sflist2stanfit(sflist)
 
   traceplot(stanfit)
+
+  out <- list(event = t(hlt),
+              stanfit = stanfit)
+
   sink(file = out_path, append = TRUE)
     cat('\n\n\n')
-    print(t(hlt))
-    cat('\n')
-    print(stanfit)
+    print(out)
   sink()
 }
