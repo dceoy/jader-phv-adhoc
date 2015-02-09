ba69387c5fbc7ad46b33036f438dae7d3de70b53
dceoy <d.narsil@gmail.com>
2015-02-10 02:04:20 +0900

    Optimize screening processes

---
+++ dm_tbl.R
@@ -0,0 +1,197 @@
+# coding: utf-8
+#
+#                event
+#              +       -
+#          +-------+-------+
+#        + |   a   |   b   | a + b
+#  drug    +-------+-------+
+#        - |   c   |   d   | c + d
+#          +-------+-------+
+#            a + c   b + d
+#
+# Preset DATABASE PATH as 'db'
+
+
+dt_pkgs <- c('RSQLite', 'dplyr', 'data.table', 'snow')
+sapply(dt_pkgs, function(p) require(p, character.only = TRUE))
+
+if (! 'cl' %in% objects()) cl <- makeCluster(4, type = 'SOCK')
+if (! 'db' %in% objects()) db <- 'mj.sqlite3'
+
+con <- dbConnect(dbDriver('SQLite'), db)
+
+sql_base <- 'SELECT
+               d.case_id AS case_id,
+               suspected,
+               CASE
+                 WHEN age == "10歳未満" THEN 0
+                 WHEN age LIKE "%歳代" THEN REPLACE(age, "歳代", "")
+               END AS age,
+               CASE
+                 WHEN sex == "女性" THEN 0
+                 WHEN sex == "男性" THEN 1
+               END AS sex,
+               CASE
+                 WHEN d.case_id IN (
+                   SELECT DISTINCT case_id FROM drug WHERE name IN (
+                     SELECT DISTINCT drug FROM d_class WHERE class == "dpp4_inhibitor"
+                   )
+                 ) THEN 1
+                 ELSE 0
+               END AS dpp4_inhibitor,
+               CASE
+                 WHEN d.case_id IN (
+                   SELECT DISTINCT case_id FROM drug WHERE name IN (
+                     SELECT DISTINCT drug FROM d_class WHERE class == "glp1_agonist"
+                   )
+                 ) THEN 1
+                 ELSE 0
+               END AS glp1_agonist
+             FROM
+               demo d
+             INNER JOIN
+               (
+                 SELECT
+                   case_id,
+                   name AS suspected
+                 FROM
+                   drug
+                 WHERE
+                   sn == 1
+               ) s ON s.case_id == d.case_id
+             WHERE
+               age LIKE "%0歳%" AND
+               sex IN ("男性", "女性") AND
+               d.case_id IN (
+                 SELECT DISTINCT
+                   case_id
+                 FROM
+                   ade10
+                 WHERE
+                   drug_start_date != "" AND
+                   drug IN (
+                     SELECT DISTINCT drug FROM d_class
+                   )
+                 GROUP BY
+                   case_id, drug
+                 HAVING
+                   MIN(drug_start_date) LIKE "201_" OR
+                   MIN(drug_start_date) LIKE "201___" OR
+                   MIN(drug_start_date) LIKE "201_____"
+               );'
+
+sql_hlts <- 'SELECT DISTINCT
+               a.hlt_code AS hlt_code,
+               hlt_name,
+               hlt_kanji,
+               COUNT(DISTINCT a.case_id) AS case_count
+             FROM
+               ade10 a
+             INNER JOIN
+               hlt h ON h.hlt_code == a.hlt_code
+             INNER JOIN
+               hlt_j hj ON hj.hlt_code == a.hlt_code
+             GROUP BY
+               a.hlt_code;'
+
+sql_reac <- 'SELECT DISTINCT
+               case_id,
+               hlt_code
+             FROM
+               ade10;'
+
+sql_hist <- 'SELECT DISTINCT
+               case_id,
+               hlt_code
+             FROM
+               hist h
+             INNER JOIN
+               pt_j p ON p.pt_kanji == h.disease
+             INNER JOIN
+               hlt_pt hp ON hp.pt_code == p.pt_code
+             WHERE
+               case_id IN (
+                 SELECT DISTINCT case_id FROM ade10
+               );'
+
+sql_ccmt <- 'SELECT DISTINCT
+               case_id,
+               drug
+             FROM
+               ade10
+             WHERE
+               drug NOT IN (
+                 SELECT DISTINCT drug FROM d_class WHERE class IN ("dpp4_inhibitor", "glp1_agonist")
+               );'
+
+sql_sgnl <- 'SELECT
+               t123.drug AS drug,
+               t123.hlt_code AS hlt_code,
+               a,
+               a_b - a AS b,
+               a_c - a AS c,
+               t - a_b - a_c + a AS d
+             FROM
+               (
+                 (
+                   (
+                     SELECT drug, hlt_code, COUNT(DISTINCT case_id) AS a FROM ade10 GROUP BY drug, hlt_code
+                   ) t1 INNER JOIN (
+                     SELECT drug, COUNT(DISTINCT case_id) AS a_b FROM ade10 GROUP BY drug
+                   ) t2 ON t1.drug == t2.drug
+                 ) t12 INNER JOIN (
+                   SELECT hlt_code, COUNT(DISTINCT case_id) AS a_c FROM ade10 GROUP BY hlt_code
+                 ) t3 ON t12.hlt_code == t3.hlt_code
+               ) t123 INNER JOIN (
+                 SELECT COUNT(DISTINCT case_id) AS t FROM ade10
+               )
+             WHERE
+               a != 0 AND b != 0 AND c != 0 AND d != 0;'
+
+dt_base <- tbl_dt(data.table(dbGetQuery(con, sql_base)))
+dt_hlts <- tbl_dt(data.table(dbGetQuery(con, sql_hlts)))
+dt_reac <- tbl_dt(data.table(dbGetQuery(con, sql_reac)))
+dt_hist <- tbl_dt(data.table(dbGetQuery(con, sql_hist)))
+dt_ccmt <- tbl_dt(data.table(dbGetQuery(con, sql_ccmt)))
+dt_sgnl <- tbl_dt(data.table(dbGetQuery(con, sql_sgnl)))
+
+dt_reac <- dt_reac %>% filter(case_id %in% filter(dt_base, dpp4_inhibitor == 1 | glp1_agonist == 1)$case_id)
+dt_hlts <- dt_hlts %>% filter(hlt_code %in% unique(dt_reac$hlt_code))
+dt_hist <- dt_hist %>% filter(case_id %in% dt_base$case_id, hlt_code %in% dt_hlts$hlt_code)
+dt_ccmt <- dt_ccmt %>% filter(case_id %in% dt_base$case_id)
+
+dt_base <- dt_base %>%
+             mutate(age = as.integer(age)) %>%
+             mutate(sex = as.integer(sex)) %>%
+             mutate(dpp4_inhibitor = as.integer(dpp4_inhibitor)) %>%
+             mutate(glp1_agonist = as.integer(glp1_agonist))
+
+fex <- function(t) {
+  f <- fisher.test(matrix(t, nrow = 2), alternative = 'two.sided', conf.level = 0.95)
+  p_or <- append(c(f$p.value, f$estimate), f$conf.int)
+  names(p_or) <- c('p_val', 'f_or', 'f_ll95', 'f_ul95')
+  return(p_or)
+}
+
+dt_sgnl <- dt_sgnl %>%
+             select(a:d) %>%
+             parApply(cl, ., 1, fex) %>%
+             t() %>%
+             cbind(dt_sgnl) %>%
+             filter(p_val < 0.05, f_or > 1) %>%
+             select(drug, hlt_code)
+
+#dt_sgnl <- dt_sgnl %>%
+#             mutate(ror_ll95 = exp(log(a * d / c / b) - 1.96 * sqrt(1 / a + 1 / b + 1 / c + 1 / d))) %>%
+#             filter(ror_ll95 > 1) %>%
+#             select(drug, hlt_code)
+
+tables()
+#      NAME      NROW NCOL MB COLS                                                  KEY
+# [1,] dt_base  6,897    6  1 case_id,suspected,age,sex,dpp4_inhibitor,glp1_agonist
+# [2,] dt_ccmt 55,700    2  2 case_id,drug
+# [3,] dt_hist 29,492    2  1 case_id,hlt_code
+# [4,] dt_hlts    628    4  1 hlt_code,hlt_name,hlt_kanji,case_count
+# [5,] dt_reac  8,011    2  1 case_id,hlt_code
+# [6,] dt_sgnl 70,930    2  2 drug,hlt_code
+# Total: 8MB
--- sc_glm.R
+++ sc_glm.R
@@ -27,20 +27,29 @@ select <- dplyr::select
 cl <- makeCluster(detectCores(), type = 'SOCK')
 registerDoSNOW(cl)
 .Last <- function() stopCluster(cl)
-db <- 'mj.sqlite3'
-source('sc_tables.R')
 
+db <- 'mj.sqlite3'
+source('sc_tbl.R')
+#      NAME       NROW NCOL MB COLS                                   KEY
+# [1,] dt_base 165,779    5 14 case_id,suspected,age,sex,incretin
+# [2,] dt_ccmt 850,878    2 23 case_id,drug
+# [3,] dt_hist 427,258    2 12 case_id,hlt_code
+# [4,] dt_hlts     703    4  1 hlt_code,hlt_name,hlt_kanji,case_count
+# [5,] dt_reac  12,570    2  1 case_id,hlt_code
+# [6,] dt_sgnl  70,930    2  2 drug,hlt_code
+# Total: 53MB
 
 stdout_path <- paste('output/sc_glm_', gsub('[ :]', '-', Sys.time()), '.txt', sep = '')
 signal_path <- 'output/sc_glm_signal.txt'
-or_csv_path <- 'output/sc_glm_orci.csv'
+csv_path <- 'output/sc_glm_orci.csv'
 cat('glm\n', file = stdout_path)
 cat('glm signal\n', file = signal_path)
-dt_orci <- data.table()
+cat('', file = csv_path)
 
 foreach (code = dt_hlts$hlt_code, .packages = pkgs) %dopar% {
   hlt <- dt_hlts %>% filter(hlt_code == code)
   reac <- dt_reac %>% filter(hlt_code == code)
+  hist <- dt_hist %>% filter(hlt_code == code)
   sgnl <- dt_sgnl %>% filter(hlt_code == code)
   ccmt <- dt_ccmt %>%
             filter(drug %in% sgnl$drug) %>%
@@ -49,11 +58,14 @@ foreach (code = dt_hlts$hlt_code, .packages = pkgs) %dopar% {
 
   dt <- dt_base %>%
           left_join(ccmt, by = 'case_id') %>%
-          mutate(concomit = ifelse(is.na(concomit), 0, concomit)) %>%
-          mutate(event = as.integer(ifelse(case_id %in% reac$case_id, 1, 0)))
+          mutate(concomit = as.integer(ifelse(is.na(concomit), 0, concomit))) %>%
+          mutate(preexist = as.integer(ifelse(case_id %in% hist$case_id, 1, 0))) %>%
+          mutate(event = as.integer(ifelse(case_id %in% reac$case_id, 1, 0))) %>%
+          select(event, incretin, concomit, preexist, age, sex)
 
   fit <- glm(event ~ incretin +
                      concomit +
+                     preexist +
                      age +
                      sex,
              data = dt, family = binomial)
@@ -62,27 +74,23 @@ foreach (code = dt_hlts$hlt_code, .packages = pkgs) %dopar% {
   ci <- confint(fit, level = 0.99)
   ors <- exp(cbind(s$coefficients[,1], ci[,1:2]))
   colnames(ors) <- c('OR', 'LL', 'UL')
-  p <- list(event = t(hlt),
-            summary = s,
-            odds_ratio = ors)
+  out <- list(event = t(hlt),
+              summary = s,
+              odds_ratio = ors,
+              hlt = hlt,
+              incretin_or = paste('HLT', hlt$hlt_code, ';', ors[2,1], '[', ors[2,2], '-', ors[2,3], ']'))
 
   sink(file = stdout_path, append = TRUE)
     cat('\n\n\n')
-    print(p)
+    print(out)
   sink()
-  if (ors[2,2] > 1) {
-    p <- list(p,
-              incretin_or = paste('HLT', hlt$hlt_code, ';', ors[2,1], '[', ors[2,2], '-', ors[2,3], ']'),
-              count = hlt$case_count,
-              hlt = paste(hlt$hlt_name, hlt$hlt_kanji))
 
+  if (ors[2,2] > 1) {
     sink(file = signal_path, append = TRUE)
       cat('\n\n\n')
-      print(p)
+      print(out)
     sink()
 
-    dt_orci <- dt_orci %>% rbind(c(ors[2,], c(hlt)))
+    write.table(matrix(c(ors[2,], hlt), nrow = 1), file = csv_path, append = TRUE, sep = ',', row.names = FALSE, col.names = FALSE)
   }
 }
-
-write.csv(dt_orci, file = or_csv_path, row.names = FALSE)
