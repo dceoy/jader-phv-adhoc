6af2b5e8290775671c8937184f615048984919ca
dceoy <d.narsil@gmail.com>
2015-03-16 17:18:51 +0900

    Tweak visualization

--- stan.R
+++ stan.R
@@ -27,7 +27,7 @@ if (file.exists(data_path <- 'output/dm_tbl.Rdata')) {
 
 out_path <- 'output/stan_log.txt'
 cat('', file = out_path)
-
+rdata <- list()
 hlt_codes <- yaml.load_file('hlts.yml')$stan
 
 st_model <- stan_model(file = 'car.stan')
@@ -48,7 +48,7 @@ foreach (code = hlt_codes) %do% {
           mutate(concomit = as.integer(ifelse(is.na(concomit), 0, concomit))) %>%
           mutate(preexist = as.integer(ifelse(case_id %in% hist$case_id, 1, 0))) %>%
           mutate(event = as.integer(ifelse(case_id %in% reac$case_id, 1, 0))) %>%
-          select(event, dpp4_inhibitor, glp1_agonist, concomit, preexist, age, sex, suspected, quarter)
+          select(event, dpp4_inhibitor, glp1_agonist, concomit, preexist, age, sex, quarter)
 
   ae_dat <- list(N = dt %>% nrow(),
                  M = 6,
@@ -63,14 +63,13 @@ foreach (code = hlt_codes) %do% {
                        .$q_id)
 
   sflist <- foreach(i = 1:8, .packages = 'rstan') %dopar% {
-              sampling(object = st_model, data = ae_dat, iter = 1000, chains = 1, chain_id = i, refresh = -1)
+              sampling(object = st_model, data = ae_dat, iter = 2000, chains = 1, chain_id = i, refresh = -1)
             }
   stanfit <- sflist2stanfit(sflist)
 
   plot_path <- paste('img/plot_', hlt$hlt_code, '.pdf', sep = '')
   traceplot_path <- paste('img/traceplot_', hlt$hlt_code, '.pdf', sep = '')
   violin_path <- paste('img/violin_', hlt$hlt_code, '.svg', sep = '')
-  rdata_path <- paste('output/stan_', hlt$hlt_code, '.Rdata', sep = '')
 
   pdf(plot_path)
     plot(stanfit)
@@ -87,31 +86,37 @@ foreach (code = hlt_codes) %do% {
   N_mc <- length(la$alpha)
 
   bs <- tbl_dt(data.table(la$beta))
-  bs <- bs %>% setnames(c('dpp4i', 'glp1a', 'ccm', 'pre', 'age', 'sex'))
-  bs_g <- bs %>%
+  ors <- bs %>%
+           setnames(c('dpp4i', 'glp1a', 'ccm', 'pre', 'age', 'sex')) %>%
+           exp()
+  or_g <- ors %>%
             select(dpp4i, glp1a) %>%
             gather(param, value)
-  bs_gq <- bs_g %>%
+  or_gq <- or_g %>%
              group_by(param) %>%
              summarize(ymed = median(value),
                        ymin = quantile(value, prob = alpha / 2),
                        ymax = quantile(value, prob = 1 - alpha / 2))
-  bs_g <- bs_g %>% inner_join(bs_gq, by = 'param')
+  or_g <- or_g %>% inner_join(or_gq, by = 'param')
 
-  p <- ggplot(bs_g, aes(x = param, y = ymed, group = param, ymax = ymax, ymin = ymin, color = param)) +
+  p <- ggplot(or_g, aes(x = param, y = ymed, group = param, ymax = ymax, ymin = ymin, color = param)) +
+         geom_hline(aes(yintercept=1), colour='#4400FF', linetype=2) +
          geom_violin(trim = FALSE, fill = '#5B423D', linetype = 'blank', alpha = I(1/3)) +
-         geom_pointrange(data = bs_gq, size = 0.75) +
+         geom_pointrange(data = or_gq, size = 0.75, shape = 15) +
+         scale_y_log10(limits = c(0.1, 200)) +
+         scale_x_discrete(limits = rev(c('dpp4i', 'glp1a'))) +
+         scale_colour_manual(values=c('#FF00FF', '#4400FF')) +
          labs(x = '', y = '') +
-         theme(axis.text.x = element_text(size = 14), axis.text.y = element_text(size = 14))
+         theme(axis.text.x = element_text(size = 14), axis.text.y = element_text(size = 14), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background=element_rect(fill='#DDDDFF')) +
+         coord_flip()
 
   svg(violin_path, width = 12, height = 8)
     print(p)
   dev.off()
 
-  orci <- bs %>%
+  orci <- ors %>%
             apply(2, function(b) quantile(b, c(0.5, alpha / 2, 1 - alpha / 2))) %>%
-            t() %>%
-            exp()
+            t()
 
   out <- list(event = t(hlt), stanfit = stanfit, odds_ratio = orci, dpp4i = '', glp1a = '')
   if (orci[1,2] > 1) out$dpp4i <- 'significant'
@@ -121,5 +126,7 @@ foreach (code = hlt_codes) %do% {
     print(out)
   sink()
 
-  save.image(rdata_path)
+  rdata[paste('or', code, sep = '_')] <- ors
 }
+
+save.image('output/stan.Rdata')
