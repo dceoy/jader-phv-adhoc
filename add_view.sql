ae8ff30e94867b5137208ff44bdd9f9c2b3898fc
dceoy <d.narsil@gmail.com>
2015-12-28 01:47:01 +0900

    Add add_view.sql

---
+++ add_view.sql
@@ -0,0 +1,45 @@
+CREATE VIEW ade AS
+  SELECT DISTINCT
+    dr.name AS drug,
+    pj.pt_kanji AS pt_kanji,
+    pj.pt_code AS pt_code,
+    p.pt_soc_code AS soc_code,
+    dr.case_id AS case_id,
+    CASE
+      WHEN quarter LIKE "%・第一" THEN REPLACE(quarter, "・第一", "q1")
+      WHEN quarter LIKE "%・第二" THEN REPLACE(quarter, "・第二", "q2")
+      WHEN quarter LIKE "%・第三" THEN REPLACE(quarter, "・第三", "q3")
+      WHEN quarter LIKE "%・第四" THEN REPLACE(quarter, "・第四", "q4")
+    END AS quarter,
+    CASE
+      WHEN age == "10歳未満" THEN 0
+      WHEN age LIKE "%歳代" THEN REPLACE(age, "歳代", "")
+    END AS age,
+    CASE
+      WHEN sex == "女性" THEN 0
+      WHEN sex == "男性" THEN 1
+    END AS sex
+  FROM
+    drug dr
+  INNER JOIN
+    reac re ON re.case_id == dr.case_id
+  INNER JOIN
+    demo de ON de.case_id == dr.case_id
+  INNER JOIN
+    pt_j pj ON pj.pt_kanji == re.event
+  LEFT OUTER JOIN
+    pt p ON p.pt_code == pj.pt_code
+  WHERE
+    age LIKE "%0歳%" AND
+    sex IN ("男性", "女性") AND
+    (quarter LIKE '2004%' OR
+     quarter LIKE '2005%' OR
+     quarter LIKE '2006%' OR
+     quarter LIKE '2007%' OR
+     quarter LIKE '2008%' OR
+     quarter LIKE '2009%' OR
+     quarter LIKE '2010%' OR
+     quarter LIKE '2011%' OR
+     quarter LIKE '2012%' OR
+     quarter LIKE '2013%' OR
+     quarter LIKE '2014%');
