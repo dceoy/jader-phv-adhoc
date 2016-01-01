c38d642fbe21e1814c85cd1da3f956aecc992476
dceoy <d.narsil@gmail.com>
2016-01-01 17:37:51 +0900

    Save memories

--- hglm.R
+++ hglm.R
@@ -1,73 +1,45 @@
 #!/usr/bin/env Rscript
 
-source('prep_tables.R') # dt_ade, dt_soc, dt_bf
 sapply(c('dplyr', 'tidyr', 'data.table', 'snow', 'rstan', 'ggmcmc'), require, character.only = TRUE)
 select <- dplyr::select
+if(length(v_socc <- as.integer(commandArgs(trailingOnly = TRUE))) == 0) {
+  v_socc <- tbl_dt(fread('output/csv/dt_soc.csv'))$soc_code
+}
+print(v_socc)
 
-v_yid <- 1:length(unique(dt_ade$year))
-names(v_yid) <- sort(unique(dt_ade$year))
-
-gen_ls_d_by <- function(socc, dt_base, bf_threshold = 100) {
-  sapply(c('dplyr', 'tidyr', 'data.table'), require, character.only = TRUE)
-  dt_susp <- dt_base %>%
-    filter(soc_code == socc, bf > bf_threshold) %>%
-    group_by(case_id) %>%
-    summarize(drug_count = n_distinct(drug))
-  dt_stan <- dt_base %>%
-    group_by(case_id, age, sex, yid) %>%
-    summarize(event = ifelse(socc %in% soc_code, 1, 0)) %>%
-    tbl_dt() %>%
-    left_join(dt_susp, by = 'case_id') %>%
-    replace_na(list(drug_count = 0)) #%>% slice(., sample(1:nrow(.), 100))
-  return(list(soc_code = socc,
-              data = list(N = nrow(dt_stan),
-                          M = 3,
-                          y = dt_stan$event,
-                          x = select(dt_stan, drug_count, age, sex),
-                          L = length(unique(dt_stan$yid)),
-                          t = dt_stan$yid)))
+hglm_ic <- function(socc, models, rdata_dir = 'output/rdata/') {
+  stan_waic <- function(model, data, tag) {
+    waic <- function(ll) {
+      return(- mean(log(colMeans(exp(ll)))) + mean(colMeans(ll ^ 2) - colMeans(ll) ^ 2))
+    }
+    stanfit <- sampling(model, data = data, chains = 2, iter = 2000, warmup = 1000)
+    save(stanfit, file = paste0(rdata_dir, 'stanfit_', tag, '.Rdata'))
+    pdf(paste0('output/img/traceplot_', tag, '.pdf')); traceplot(stanfit); dev.off()
+    pdf(paste0('output/img/traceplot_', tag, '.pdf')); plot(stanfit); dev.off()
+    ggmcmc(ggs(stanfit), file = paste0('output/img/ggmcmc_', tag, '.pdf'))
+    sink(paste0('output/log/stanfit_', tag, '.txt')); print(stanfit); sink()
+    return(waic(extract(stanfit)$log_lik))
+  }
+  ls_d <- fread(paste0('output/csv/dt_', socc, '.csv')) %>%
+    list(N = nrow(.),
+         M = 3,
+         y = .$event,
+         x = select(., drug_count, age, sex),
+         L = length(unique(.$yid)),
+         t = .$yid)
+  write.table(dt_waic <- data.table(soc_code = socc,
+                                    fixed = stan_waic(models$fixed,
+                                                      ls_d[c('N', 'M', 'y', 'x')],
+                                                      paste0('fixed_', socc)),
+                                    mixed = stan_waic(models$mixed,
+                                                      ls_d,
+                                                      paste0('fixed_', socc))),
+              file = paste0('waic_', socc, '.csv'), sep = ',', row.names = FALSE)
+  return(dt_waic)
 }
 
-cl <- makeCluster(parallel::detectCores(), type = 'SOCK')
-ls_ls_d <- parLapply(cl,
-                     dt_soc$soc_code,
-                     gen_ls_d_by,
-                     dt_base = dt_ade %>%
-                       mutate(age = as.integer(age), yid = v_yid[as.character(year)]) %>%
-                       inner_join(dt_bf, by = c('drug', 'soc_code')) %>%
-                       select(case_id, drug, soc_code, age, sex, yid, bf))
-models <- parLapply(cl, c(fixed = 'fixed.stan', mixed = 'mixed.stan'), stan_model)
+cl <- makeCluster(2, type = 'SOCK')
+system.time(models <- parLapply(cl, c(fixed = 'fixed.stan', mixed = 'mixed.stan'), stan_model))
 stopCluster(cl)
 
-hglm_ic <- function(l, models) {
-  stan_waic <- function(model, data, tag) {
-    waic <- function(loglik) {
-      return(- mean(log(colMeans(exp(loglik)))) + mean(colMeans(loglik ^ 2) - colMeans(loglik) ^ 2))
-    }
-
-    f <- sampling(model, data = data, chains = 4, iter = 2000, warmup = 1000)
-    save(f, file = paste0('output/rdata/stanfit_', tag, '.Rdata'))
-    pdf(paste0('output/img/traceplot_', tag, '.pdf')); traceplot(f); dev.off()
-    pdf(paste0('output/img/traceplot_', tag, '.pdf')); plot(f); dev.off()
-    ggmcmc(ggs(f), file = paste0('output/img/ggmcmc_', tag, '.pdf'))
-    sink(paste0('output/log/stanfit_', tag, '.txt')); print(f); sink()
-    return(waic(extract(f)$log_lik))
-  }
-  sapply(c('dplyr', 'data.table', 'rstan', 'ggmcmc'), require, character.only = TRUE)
-  rstan_options(auto_write = TRUE); options(mc.cores = 4)
-  return(data.table(soc_code = l$soc_code,
-                    model = c('fixed', 'mixed'),
-                    waic = c(stan_waic(models$fixed,
-                                       l$data[c('N', 'M', 'y', 'x')],
-                                       paste0('fixed_', l$soc_code)),
-                             stan_waic(models$mixed,
-                                       l$data[c('N', 'M', 'y', 'x', 'L', 't')],
-                                       paste0('fixed_', l$soc_code)))))
-}
-
-#cl <- makeCluster(floor(parallel::detectCores() / 4), type = 'SOCK')
-#print(system.time(dt_ic <- bind_rows(parLapply(cl, ls_ls_d, hglm_ic))))
-print(system.time(dt_ic <- bind_rows(lapply(ls_ls_d, hglm_ic, models = models))))
-#stopCluster(cl)
-
-sink('out/log/stan_log.txt'); lapply(results, print); sink()
+system.time(lapply(v_socc, hglm_ic, models = models, rdata_dir = '~/rdata/'))
--- prep_tables.R
+++ prep_tables.R
@@ -9,82 +9,105 @@
 #          +-------+-------+
 #
 
-if (file.exists(table_data_path <- 'output/rdata/tables.Rdata')) {
-  load(table_data_path)
-} else {
-  sapply(c('dplyr', 'data.table', 'RSQLite', 'snow', 'Rcpp'), require, character.only = TRUE)
-  select <- dplyr::select
-  connect_db <- function(file, type = 'SQLite') return(dbConnect(dbDriver(type), file))
-  sql_dt <- function(con, sql) return(tbl_dt(as.data.table(dbGetQuery(con, sql))))
+sapply(c('dplyr', 'data.table', 'RSQLite', 'snow', 'Rcpp'), require, character.only = TRUE)
+select <- dplyr::select
+connect_db <- function(file, type = 'SQLite') return(dbConnect(dbDriver(type), file))
+sql_dt <- function(con, sql) return(tbl_dt(as.data.table(dbGetQuery(con, sql))))
 
-  sql_ade = 'SELECT DISTINCT
-               drug,
-               soc_code,
-               case_id,
-               year,
-               age,
-               sex
-             FROM
-               ade;'
-  sql_soc = 'SELECT DISTINCT
-               s.soc_code AS soc_code,
-               soc_name,
-               soc_kanji
-             FROM
-               soc s
-             INNER JOIN
-               soc_j sj ON sj.soc_code == s.soc_code;'
-  sql_ct22 = 'SELECT
-                t123.drug AS drug,
-                t123.soc_code AS soc_code,
-                a,
-                a_b - a AS b,
-                a_c - a AS c,
-                t - a_b - a_c + a AS d
-              FROM
+sql_ade = 'SELECT DISTINCT
+             drug,
+             soc_code,
+             case_id,
+             year,
+             age,
+             sex
+           FROM
+             ade;'
+sql_soc = 'SELECT DISTINCT
+             s.soc_code AS soc_code,
+             soc_name,
+             soc_kanji
+           FROM
+             soc s
+           INNER JOIN
+             soc_j sj ON sj.soc_code == s.soc_code;'
+sql_ct22 = 'SELECT
+              t123.drug AS drug,
+              t123.soc_code AS soc_code,
+              a,
+              a_b - a AS b,
+              a_c - a AS c,
+              t - a_b - a_c + a AS d
+            FROM
+              (
                 (
                   (
-                    (
-                      SELECT drug, soc_code, COUNT(DISTINCT case_id) AS a FROM ade GROUP BY drug, soc_code
-                    ) t1
-                    INNER JOIN (
-                      SELECT drug, COUNT(DISTINCT case_id) AS a_b FROM ade GROUP BY drug
-                    ) t2 ON t2.drug == t1.drug
-                  ) t12
+                    SELECT drug, soc_code, COUNT(DISTINCT case_id) AS a FROM ade GROUP BY drug, soc_code
+                  ) t1
                   INNER JOIN (
-                    SELECT soc_code, COUNT(DISTINCT case_id) AS a_c FROM ade GROUP BY soc_code
-                  ) t3 ON t3.soc_code == t12.soc_code
-                ) t123
-              INNER JOIN (
-                SELECT COUNT(DISTINCT case_id) AS t FROM ade
-              );'
+                    SELECT drug, COUNT(DISTINCT case_id) AS a_b FROM ade GROUP BY drug
+                  ) t2 ON t2.drug == t1.drug
+                ) t12
+                INNER JOIN (
+                  SELECT soc_code, COUNT(DISTINCT case_id) AS a_c FROM ade GROUP BY soc_code
+                ) t3 ON t3.soc_code == t12.soc_code
+              ) t123
+            INNER JOIN (
+              SELECT COUNT(DISTINCT case_id) AS t FROM ade
+            );'
 
-  append_bayes_factor <- function(dt, cl) {
-    par_cal_bf <- function(dt, cl) {
-      divide_dt <- function(dt, k) {
-        return(lapply(1:k, function(i) return(filter(dt, i == rep(1:k, nrow(dt))))))
-      }
-      cal_bf <- function(d) {
-        Rcpp::sourceCpp('bayes_factor.cpp')
-        return(as.data.frame(t(apply(d, 1, bayes_factor))))
-      }
-      return(as.data.table(bind_rows(parLapply(cl, divide_dt(dt, length(cl)), cal_bf))))
+append_bayes_factor <- function(dt, cl) {
+  par_cal_bf <- function(dt, cl) {
+    divide_dt <- function(dt, k) {
+      return(lapply(1:k, function(i) return(filter(dt, i == rep(1:k, nrow(dt))))))
     }
-
-    return(inner_join(dt,
-                      par_cal_bf(distinct(select(dt, a:d)), cl),
-                      by = c('a', 'b', 'c', 'd')))
+    cal_bf <- function(d) {
+      Rcpp::sourceCpp('bayes_factor.cpp')
+      return(as.data.frame(t(apply(d, 1, bayes_factor))))
+    }
+    return(as.data.table(bind_rows(parLapply(cl, divide_dt(dt, length(cl)), cal_bf))))
   }
 
-  cl <- makeCluster(parallel::detectCores(), type = 'SOCK')
-  con <- connect_db('meddra_jader.sqlite3')
-
-  system.time(dt_ade <- sql_dt(con, sql_ade)) %>% print()
-  system.time(dt_soc <- sql_dt(con, sql_soc)) %>% print()
-  system.time(dt_bf <- append_bayes_factor(sql_dt(con, sql_ct22), cl)) %>% print()
-
-  dbDisconnect(con)
-  stopCluster(cl)
-
-  save(dt_ade, dt_soc, dt_bf, file = table_data_path)
+  return(inner_join(dt,
+                    par_cal_bf(distinct(select(dt, a:d)), cl),
+                    by = c('a', 'b', 'c', 'd')))
 }
+
+cl <- makeCluster(parallel::detectCores(), type = 'SOCK')
+
+con <- connect_db('meddra_jader.sqlite3')
+system.time(dt_ade <- sql_dt(con, sql_ade)) %>% print()
+system.time(write.table(dt_soc <- sql_dt(con, sql_soc), file = 'output/csv/dt_soc.csv', row.names = FALSE)) %>% print()
+system.time(dt_bf <- append_bayes_factor(sql_dt(con, sql_ct22), cl)) %>% print()
+dbDisconnect(con)
+
+v_yid <- 1:length(unique(dt_ade$year))
+names(v_yid) <- sort(unique(dt_ade$year))
+
+write_dt_by_soc <- function(socc, dt, bf_threshold = 100) {
+  if(! file.exists(path <- paste0('output/csv/dt_', socc, '.csv'))) {
+    sapply(c('dplyr', 'tidyr', 'data.table'), require, character.only = TRUE)
+    dt_susp <- dt %>%
+      filter(soc_code == socc, bf > bf_threshold) %>%
+      group_by(case_id) %>%
+      summarize(drug_count = n_distinct(drug))
+    dt %>%
+      group_by(case_id, age, sex, yid) %>%
+      summarize(event = ifelse(socc %in% soc_code, 1, 0)) %>%
+      tbl_dt() %>%
+      left_join(dt_susp, by = 'case_id') %>%
+      replace_na(list(drug_count = 0)) %>%
+      select(event, drug_count, age, sex, yid) %>%
+      write.table(file = path, sep = ',', row.names = FALSE)
+  }
+}
+
+system.time(parLapply(cl,
+                      dt_soc$soc_code,
+                      write_dt_by_soc,
+                      dt = dt_ade %>%
+                        mutate(age = as.integer(age), yid = v_yid[as.character(year)]) %>%
+                        inner_join(dt_bf, by = c('drug', 'soc_code')) %>%
+                        select(case_id, drug, soc_code, age, sex, yid, bf))) %>% print()
+
+stopCluster(cl)
